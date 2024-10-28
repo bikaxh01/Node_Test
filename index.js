6a113f585089153b13dbf368132217934e2e8748
Bikash Mishra <mishrabikash202@gmail.com>
2024-10-29 02:33:47 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-import express from 'express'
+import express from "express";
 
-const port = 3000
-const app = express()
-
-app.get('/',(req,res)=>{
-    res.json("Server is running")
-})
+const port = 3000;
+const app = express();
+app.set('trust proxy',true)
 
 
-app.listen(port,(()=>console.log(`Running at ${port}`)))
+app.get("/", (req, res) => {
+
+    console.log("Request Camed");
+    const Address = req.socket.remoteAddress
+  console.log("🚀 ~ app.get ~ req:", Address);
+
+  res.status(200).json({message:"Server is running",ip:Address});
+});
+
+app.listen(port, () => console.log(`Running at ${port}`));
