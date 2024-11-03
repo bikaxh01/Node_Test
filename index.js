54114941641f62e30986c3ad2a2bc1628dca896f
Bikash Mishra <mishrabikash202@gmail.com>
2024-11-04 05:04:28 +0530

    webhook test

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const port = 3000;
 const app = express();
-app.set('trust proxy',true)
-
+app.set("trust proxy", true);
+app.use(bodyParser())
 
 app.get("/", (req, res) => {
-
-    console.log("Request Camed");
-    const Address = req.socket.remoteAddress
+  console.log("Request Camed");
+  const Address = req.socket.remoteAddress;
   console.log("🚀 ~ app.get ~ req:", Address);
 
-  res.status(200).json({message:"Server is running",ip:Address});
+  res.status(200).json({ message: "Server is running", ip: Address });
+});
+
+app.post("/github", async (req, res) => {
+  const body = req.body;
+
+  console.log("🚀 ~ app.post ~ body:", body);
+  res.json("OK");
 });
 
 app.listen(port, () => console.log(`Running at ${port}`));
